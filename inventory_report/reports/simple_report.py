262ae067f805419b6b888e4cb87f22461f1973bd
Williamns <williamnstolb@gmail.com>
2022-06-22 16:06:08 -0300

    Requisito 3: Corrigindo quebra de linha com \n

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -15,7 +15,7 @@ class SimpleReport:
         )
 
         return (
-                f"Data de fabricação mais antiga: {min(list_manufactures)}"
-                f"Data de validade mais próxima: {min(list_validity)}"
-                f"Empresa com mais produtos: {company_more_products}"
+                f"Data de fabricação mais antiga: {min(list_manufactures)}\n"
+                f"Data de validade mais próxima: {min(list_validity)}\n"
+                f"Empresa com mais produtos: {company_more_products}\n"
         )
