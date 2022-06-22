6182bf0c6eed314042d85291df41b225138ca43f
Williamns <williamnstolb@gmail.com>
2022-06-22 16:04:56 -0300

    Requisito 3: Criando a classe simpleReport

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,21 @@
+class SimpleReport:
+
+    def generate(product_list):
+        list_companies = list()
+        list_manufactures = list()
+        list_validity = list()
+        for product in product_list:
+            list_companies.append(product['nome_da_empresa'])
+            list_manufactures.append(product['data_de_fabricacao'])
+            list_validity.append(product['data_de_validade'])
+
+        company_more_products = max(
+          set(list_companies),
+          key=list_companies.count
+        )
+
+        return (
+                f"Data de fabricação mais antiga: {min(list_manufactures)}"
+                f"Data de validade mais próxima: {min(list_validity)}"
+                f"Empresa com mais produtos: {company_more_products}"
+        )
