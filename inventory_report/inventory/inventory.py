76ea143ece105d82c6521bd885e71ed49393e904
Williamns <williamnstolb@gmail.com>
2022-06-22 18:00:52 -0300

    Requisito 7: Gerando relatório por meio de um arquivo XML ok

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -21,14 +22,27 @@ class Inventory:
         with open(path) as file:
             return json.load(file)
 
+    def import_data_xml(path):
+        list_datas = list()
+        tree = ET.parse(path)
+        root = tree.getroot()
+        for item in root:
+            dict_datas = dict()
+            for data in item:
+                dict_datas[data.tag] = data.text
+            list_datas.append(dict_datas)
+        return list_datas
+
     def file(f):
         split_path = f.split('.')[1]
-
         if split_path == 'csv':
             return Inventory.import_data_csv(f)
 
         elif split_path == 'json':
             return Inventory.import_data_json(f)
+
+        elif split_path == 'xml':
+            return Inventory.import_data_xml(f)
 
     def import_data(path, type):
         product_list = Inventory.file(path)
