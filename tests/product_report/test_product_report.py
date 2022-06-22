009e34e63be03469518ab224f61c9b25cd8ffade
Williamns <williamnstolb@gmail.com>
2022-06-22 15:32:55 -0300

    Requisito 2: criando teste e corrigindo erro de espaçamento

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        'Redmi note 7',
+        'Xiaomi',
+        '01/03/2019',
+        '19/10/2025',
+        '19101984',
+        'longe da umidade'
+    )
+
+    assert str(product) == (
+        f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa}"
+        f" com validade até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
